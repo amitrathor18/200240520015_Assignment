1caafd1f45a6cb02352576d40cc97640be1004f1
Amit Rathor <65148863+amitrathor18@users.noreply.github.com>
2020-08-12 20:03:48 +0530

    Add files via upload

---
+++ AWP/Challenge/cal.js
@@ -0,0 +1,9 @@
+function cal() {
+  var n1 = number(document.getElementById("num1").value);
+  var n2 = number(document.getElementById("num2").value);
+  var s = 0;
+
+  if (document.getElementById("add")) {
+    s = n1 + n2;
+  }
+}
